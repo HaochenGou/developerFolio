e3de4b86da2da83496d1aee66bc4c3cd1f6b48ec
Haochen Gou <hgou@ualberta.ca>
2022-06-09 16:22:49 -0600

    Update

--- src/portfolio.js
+++ src/portfolio.js
@@ -427,7 +427,7 @@ const contactInfo = {
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
   number: "+92-0000000000",
-  email_address: "hgou.ca@gmail.com"
+  email_address: "xxx@xxx"
 };
 
 // Twitter Section
