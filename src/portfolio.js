c4a04141ee35cea5e442f98d056b203f6473c702
Haochen Gou <hgou@ualberta.ca>
2022-09-01 11:33:12 -0600

    update

--- src/portfolio.js
+++ src/portfolio.js
@@ -427,7 +427,7 @@ const contactInfo = {
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
   number: "780-449-5331",
-  email_address: "info@cwd.com",
+  email_address: "info@Canadianwebsite.ca",
   address:"426 Kaska Rd, Sherwood Park, AB T8A 4G8"
 };
 
